1941b78579029ea357e005c3c824acc2a1e0d9d2
Willany Thayse <willany.thayse@gmail.com>
2018-05-15 22:00:47 -0300

    inserção de tamanhos

--- produto.php
+++ produto.php
@@ -32,8 +32,12 @@
 						<label	for="azul">
 							<img	src="img/produtos/foto2-azul.png"	alt="produto na cor azul">
 						</label>
+						<input	type="submit"	class="comprar"	value="Comprar">
 					</fieldset>
-					<input	type="submit"	class="comprar"	value="Comprar">
+					<fieldset	class="tamanhos">
+						<legend>Escolha	o	tamanho:</legend>
+						<input	type="range"	min="36"	max="46"	value="42"	step="2"	name="tamanho"	id="tamanho">
+					</fieldset>
 				</form>
 			</div>
 			<?php 
